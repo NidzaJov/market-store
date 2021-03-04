a8bb207cf391114c1874d66eb7bb6dfd394e1b07
Nikola Jovanović <nidza.jov@gmail.com>
2021-03-04 16:10:51 +0100

    Changing CurrentCulture and CurrencyGroupSeparator for better formatting

--- DetailsPrinter.cs
+++ DetailsPrinter.cs
@@ -8,12 +8,14 @@ namespace MarketStore
         public void PrintDetails(ICard card, double purchaseValue)
         {
             PurchaseDetails details = card.ReturnPurchaseDetails(purchaseValue);
-            
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            NumberFormatInfo nfi = new CultureInfo("en-US", false).NumberFormat;
+            nfi.CurrencyGroupSeparator = "";
             Console.WriteLine(" Purchase value: {0} \n Discount rate: {1}% \n Discount: {2} \n Total: {3}", 
-                details.PurchaseValue.ToString("C2",new CultureInfo("en-US")), 
-                details.ActualDiscountRate.ToString("F1", new CultureInfo("en-US")), 
-                details.Discount.ToString("C2", new CultureInfo("en-US")), 
-                details.Total.ToString("C2", new CultureInfo("en-US")));
+                details.PurchaseValue.ToString("C2", nfi), 
+                details.ActualDiscountRate.ToString("N1"), 
+                details.Discount.ToString("C2", nfi), 
+                details.Total.ToString("C2", nfi));
 
         }
     }
